9bb7fd8daf7a3c803d7632c771d1410dd4d040a1
PJQuEz <peerapat_j@cmu.ac.th>
2016-03-28 18:06:10 +0700

    Number 8

---
+++ src/main/java/camt/se331/shoppingcart/repository/ShoppingCartRepository.java
@@ -0,0 +1,10 @@
+package camt.se331.shoppingcart.repository;
+
+import camt.se331.shoppingcart.entity.ShoppingCart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by พีรพัฒน์ on 28/3/2559.
+ */
+public interface ShoppingCartRepository extends JpaRepository<ShoppingCart,Long> {
+}
